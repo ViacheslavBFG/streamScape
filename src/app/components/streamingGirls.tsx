0a35d9fc7f7162e82888202c89a9309bf100018e
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2024-07-23 08:37:42 +0300

    fix modal

--- src/app/components/streamingGirls.tsx
+++ src/app/components/streamingGirls.tsx
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import { Button } from "antd";
-import ModalForm from "./ModalForm";
+import ModalForm from "./modalForm";
 
 const StreamingGirls = () => {
   const settings = {
