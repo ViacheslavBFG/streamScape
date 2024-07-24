8f2f5e288019bfa3fc507b5d27acc65eff88f845
ViacheslavBFG <131298066+ViacheslavBFG@users.noreply.github.com>
2024-07-24 09:23:29 +0300

    hot fix btn

--- src/app/components/btnLog.tsx
+++ src/app/components/btnLog.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const BtnLog = () => {
   return (
-    <div className="flex justify-end animate-bounce ">
+    <div className="flex justify-end animate-bounce max-sm:hidden ">
       <Button className="text-blue-400">Log in</Button>
     </div>
   );
